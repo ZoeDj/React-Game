ff06c95f441f5dd1e0b6b4d214e6f626212091c1
ZoeDj <studio3fun@gmail.com>
2019-08-31 21:56:14 -0400

    Updates

---
+++ precache-manifest.cbeaf25bace862bcc8aad3a626736d83.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18abcb4d609b4e4c0f6e70c834ae3a97",
+    "url": "/React-Game/index.html"
+  },
+  {
+    "revision": "75ec9d3d56e103e6d243",
+    "url": "/React-Game/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "a3c148d5c5181d623197",
+    "url": "/React-Game/static/css/main.0465703e.chunk.css"
+  },
+  {
+    "revision": "75ec9d3d56e103e6d243",
+    "url": "/React-Game/static/js/2.974ce047.chunk.js"
+  },
+  {
+    "revision": "a3c148d5c5181d623197",
+    "url": "/React-Game/static/js/main.2f77417c.chunk.js"
+  },
+  {
+    "revision": "ec13f55e3924d53f2de9",
+    "url": "/React-Game/static/js/runtime~main.5adbf522.js"
+  }
+]);
