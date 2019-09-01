d8f48bf70e67064e275e7f6dfcf42c0f70d6d5c8
ZoeDj <studio3fun@gmail.com>
2019-08-31 21:58:32 -0400

    published

---
+++ clicky-game/src/App.js
@@ -0,0 +1,66 @@
+import React from "react";
+import GameImage from "./components/GameImage/GameImage";
+import Wrapper from "./components/Wrapper/Wrapper";
+import images from "./images.json";
+import "./App.css";
+
+class App extends React.Component {
+  state = {
+    images,
+    clickedImages: [],
+    score: 0
+  };
+
+  shuffleImages = images => {
+    for (let i = images.length - 1; i > 0; i--) {
+      let j = Math.floor(Math.random() * (i + 1));
+      let random = images[i];
+      images[i] = images[j];
+      images[j] = random;
+    }
+    return images;
+  };
+
+  clickedImage = id => {
+    const shuffleImages = this.shuffleImages(images);
+    this.setState({ images: shuffleImages });
+    if (this.state.clickedImages.includes(id)) {
+      this.setState({ score: 0, clickedImages: [] });
+    } else {
+      this.setState({
+        clickedImages: this.state.clickedImages.concat([id]),
+        score: this.state.score + 1
+      });
+    }
+  };
+
+  render() {
+    return (
+      <div>
+        <div className="jumbotron jumbotron-fluid">
+          <div className="container">
+            <h1 className="display-4">Clicky Game</h1>
+            <h5 className="lead">
+              Click on an image to earn points. If you click on the image more
+              than once, you'll lose and the game will start over.
+            </h5>
+            <h3 className="counter">Click Count: {this.state.score}</h3>
+          </div>
+        </div>
+        <Wrapper>
+          {this.state.images.map(image => (
+            <GameImage
+              image={image.img}
+              id={image.id}
+              key={image.id}
+              clickedImage={this.clickedImage}
+              imageClick={this.imageClick}
+            />
+          ))}
+        </Wrapper>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ clicky-game/src/components/GameImage/GameImage.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./GameImage.css";
+
+function GameImage(props) {
+  return (
+    <div className="img-container">
+      <img
+        src={props.image}
+        alt={props.name}
+        onClick={() => props.clickedImage(props.id)}
+      />
+    </div>
+  );
+}
+
+export default GameImage;
